f2d0d64f4d93793e3b52651a016baa560cc70025
lugq <SSH>
2021-02-16 22:41:04 +0800

    SpringCloud Bus

---
+++ cloud-config-client3366/src/main/java/com/yango/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,20 @@
+package com.yango.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Description TODO
+ * @Author lugq
+ * @Email lugq@yango.com.cn
+ * @Date 2021/2/16 0016 21:51
+ * @Version 1.0
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class, args);
+    }
+}
---
+++ cloud-config-client3366/src/main/java/com/yango/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,33 @@
+package com.yango.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description TODO
+ * @Author lugq
+ * @Email lugq@yango.com.cn
+ * @Date 2021/2/15 0015 22:58
+ * @Version 1.0
+ */
+@RestController
+@Slf4j
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return "serverPort：" + serverPort + "\t configInfo:" + configInfo;
+    }
+
+
+}
